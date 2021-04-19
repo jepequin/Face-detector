4ec4b4daa98a4dab172ce5e2de7b28a6db90c4c9
jisep <jesua19uni@gmail.com>
2021-04-19 17:14:14 +0800

    Tuned parameter minNeighbors from detectMultiscale to reduce number of false-positive face detections. Created main function.

--- face_detector.py
+++ face_detector.py
@@ -1,7 +1,8 @@
 import os
+from time import time
 import pytesseract
 import cv2 #import OpevCV
-from PIL import Image, ImageDraw
+from PIL import Image
 from math import ceil, floor, sqrt
 
 def input_keyword():
@@ -19,11 +20,25 @@ def input_keyword():
 	return word
 
 def coordinate_change(coordinates):
+	'''Changes coordinates rectangle from (upper_left_x, upper_left_y, witdth, height) to (upper_left_x, upper_left_y, lower_right_x, lower_right_y)
+	:type coordinates: list 
+	:param coordinates: integer list of upper-left coordinates and dimensions
+
+	:rtype: list
+	:return: integer list with diagonal coordinates of rectangle
+	'''
 	coordinates[2] += coordinates[0]
 	coordinates[3] += coordinates[1]
 	return coordinates	
 
 def resize(images):
+	'''Resizes images to match smallest image dimensions 
+	:type images: list
+	:param images: list of pillow images
+
+	:rtype: list
+	:return: list of equally sized pillow images
+	'''
 	heights = list()
 	widths = list()
 	for image in images:
@@ -36,31 +51,26 @@ def resize(images):
 	images = [image.resize((base_width,base_height)) for image in images]
 	return images
 
-def build_image_dictionary(folder):
+def image_dictionary(folder):
 	'''Creates image dictionary using images in folder
 	:type folder: str
-	:param folder: path of folder containing images
+	:param folder: path to folder containing images
 
 	:rtype: dict
-	:return: dictionary whose keys are image (relative) paths, and values are pillow images
+	:return: dictionary whose keys are image (relative) paths, and values are numpy array images
 	'''
 	image_dictionary = dict()
 	for image_name in os.listdir(folder):
 		image_path = os.path.join(folder,image_name)
-		
-		#image = Image.open(image_path)
-		image = cv2.imread(image_path)
-		
-		#image = image.convert('L') # change mode to grayscale allows to capture more text
-		image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-		
+		image = cv2.imread(image_path) # load image with opencv
+		image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) # change mode to grayscale allows to capture more text	
 		image_dictionary[image_path] = image
 	return image_dictionary	
 
 def filter_by_keyword(image_dictionary,keyword):
-	''' Filters image_dictionary images according to keyword
+	'''Filters image_dictionary images according to keyword
 	:type image_dictionary: dict
-	:param image_dictionary: keys are image (relative) paths, values are pillow images	
+	:param image_dictionary: keys are image (relative) paths, values are numpy array images	
 	:type keyword: str
 	:param keyword: string used to filter entries of dictionary image_dictionary
 
@@ -70,14 +80,32 @@ def filter_by_keyword(image_dictionary,keyword):
 	keyword = keyword.lower()
 	dictionary = image_dictionary.copy()
 	for image_path in image_dictionary:
-		#image_path = 'small_img/a-0.png'
-		image = dictionary[image_path] # mode attribute shows image is RGB
-		#image.show()
-		text = pytesseract.image_to_string(image).lower()
-		#print(text)
+		image = dictionary[image_path]
+		text = pytesseract.image_to_string(image).lower() # get text from image 
 		if keyword not in text:
-			del dictionary[image_path]
+			del dictionary[image_path] # delete entry from dictionary if keyword not detected
 	return dictionary
+
+def delete_text(image):
+	'''Performs image segmentation to delete text from image 
+	:param image: image to be segmented
+	:type image: numpy array image
+
+	:return: image with text deleted 
+	:rtype: numpy array image
+	'''
+	boxes = list()
+	segmentation_data = pytesseract.image_to_data(image) # each line of this string corresponds to a potentially retrieved word
+	segmentation_data = segmentation_data.split('\n')[1:] # ignore header 
+	for data_string in segmentation_data:		
+		data_list = data_string.split('\t')
+		word_detected = data_list[-1].strip() # no word was retrieved if this stripped string is empty  
+		if word_detected:
+			coordinates = coordinate_change([int(coordinate) for coordinate in data_list[6:10]])
+			start_point = tuple(coordinates[:2])
+			end_point = tuple(coordinates[2:])
+			image = cv2.rectangle(image,start_point,end_point,color=255,thickness=-1) # thickness -1 fills rectangle shape by specified (white) color. 
+	return image
 
 def detect_faces(image):
 	'''Detects faces in image
@@ -88,46 +116,64 @@ def detect_faces(image):
 	:return: list of pillow images cotaining faces
 	'''
 	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') # xml file stored during opencv installation
-	face_coordinates = face_cascade.detectMultiScale(image)
+	face_coordinates = face_cascade.detectMultiScale(image,minNeighbors=17) # higher minNeighbors value lowers false-positives detections
 	image = Image.fromarray(image)
 	faces = list()
-	#drawing_object = ImageDraw.Draw(image)
 	for coordinates in face_coordinates:
-		coordinates = coordinate_change(coordinates)
-		#drawing_object.rectangle(coordinates,fill=None,outline='red')
+		coordinates = coordinate_change(coordinates) # detectMultiScale returns (upper_left_x, upper_left_y, witdth, height)
 		face = image.crop(coordinates)
 		faces.append(face)
 	return faces
 
 def mosaic(images):
 	'''Creates mosaic from images in list images
-	:param images: list of PIL images
+	:param images: list of pillow images
 	:type images: list
 
 	:return: mosaic image
-	:rtype: PIL image
+	:rtype: pillow image
 	'''
 	images = resize(images)
+	### Calculate mosaic's number of rows and columns  
 	nb_columns = floor(sqrt(len(images)))
 	nb_rows = ceil(len(images)/nb_columns)
-
+	### Get dimensions of images to form mosaic 
 	tesserae_width = images[0].width
 	tesserae_height = images[0].height
+	### Calculate dimenson of mosaic
 	mosaic_width = tesserae_width*nb_columns
 	mosaic_height = tesserae_height*nb_rows
+	### Create contact sheet
 	mosaic = Image.new('L',(mosaic_width,mosaic_height))
-	
 	for index in range(len(images)):
 		x_position = index % nb_columns
 		y_position = floor(index/nb_columns)
-		mosaic.paste(images[index],(x_position*tesserae_width,y_position*tesserae_height))
+		mosaic.paste(images[index],(x_position*tesserae_width,y_position*tesserae_height)) # second parameter specifies where to paste image (upper left coordinates) 
 	return mosaic
 
-#keyword = input_keyword()
-#image_dictionary = build_image_dictionary('small_img')
-#updated_dictionary = filter_by_keyword(image_dictionary,keyword)
-#print(updated_dictionary.keys())
-#faces = detect_faces(image_dictionary['small_img/a-0.png'])
-#mosaic(faces).show()
+def main():
+	keyword = input_keyword()
+	start = time()
+	print('Creating image dictionary...')
+	img_dictionary = image_dictionary('small_img')
+	end = time()
+	print('Image dictionary created in {}s'.format(end-start))
+	start = time()
+	print('Looking for instances of keyword "{}" in images'.format(keyword))
+	filtered_dictionary = filter_by_keyword(img_dictionary,keyword)
+	end = time()
+	print('Acomplished in {}s. Keyword "{}"" found in images: {}'.format(end-start,keyword,filtered_dictionary.keys()))
+	for key in updated_dictionary:
+		print('Analyzing image {}...'.format(key))
+		image = updated_dictionary[key]
+		print('Dictionary updated')
+		image = delete_text(image)
+		print('Text deleted')
+		faces = detect_faces(image)
+		print('Faces detected')
+		mosaic(faces).show()
+		break
 
+main()
+ 
 
